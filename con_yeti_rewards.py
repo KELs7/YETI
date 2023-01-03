83ef7b8727814e7914987d84b17dcb5e6bcdb749
kels7 <hiketabo@gmail.com>
2023-01-03 02:39:28 +0000

    break long lines and update comments

--- con_yeti_rewards.py
+++ con_yeti_rewards.py
@@ -17,7 +17,7 @@ def init():
     #TODO: make this contract approve DEX to spend
 
 def buy_reward_token(contract: str, fee_cover_perc: float):
-    #if nothing is passed to fee_cover_perc param we assume a default value
+    #if nothing is passed to fee_cover_perc we assume a default value
     if fee_cover_perc == None: fee_cover_perc = decimal('0.05')
     #get total YETI balance of this contract
     yeti_amount = yeti_balance[REWARDS_CONTRACT]
@@ -25,38 +25,43 @@ def buy_reward_token(contract: str, fee_cover_perc: float):
     DEX.sell(contract=YETI, token_amount=yeti_amount)
     #get total TAU balance of this contract
     rewards_currency_amount = currency_balance[REWARDS_CONTRACT]  
-    #reserve %10 to cover for present and future transaction fees
+    #reserve a percentage to cover for present and future transaction fees
     fee_cover  = rewards_currency_amount * fee_cover_perc
     currency_amount = rewards_currency_amount - fee_cover
     #buy reward token with TAU from YETI sell
     DEX.buy(contract=contract, currency_amount=currency_amount)
+    #return DEX.buy(contract=contract, currency_amount=currency_amount)
     
 @export
-def distribute_rewards(contract: str, addresses: list, holder_min: float, distribute_min: float, fee_cover_perc: float):
+def distribute_rewards(contract: str, 
+    addresses: list, holder_min: float, distribute_min: float, fee_cover_perc: float):
     #check if caller is operator
-    assert_owner()
+    assert_operator()
     #if nothing is passed to holder_min and distribute_min, we assume certain default values
     if holder_min == None: holder_min = 500_000_000
     if distribute_min == None: distribute_min = 1000
     #buy reward token for distribution
     buy_reward_token(contract=contract, fee_cover_perc=fee_cover_perc)
+    #assert 5 < 1, f'{buy_reward_token(contract=contract, fee_cover_perc=fee_cover_perc)}'
     #get total reward token balance of this contract
     rewards_token_balance = ForeignHash(foreign_contract=contract, foreign_name='balances')
     rewards_token_amount = rewards_token_balance[REWARDS_CONTRACT]
     #check minimum reward token amount required for distribute 
-    assert rewards_token_amount >= distribute_min, f'Not enough funds to distribute! Current amount is {rewards_token_amount}'
+    assert rewards_token_amount >= distribute_min, \
+        f'Not enough funds to distribute! Current amount is {rewards_token_amount}'
     
     reward_token = I.import_module(contract)
     
     for address in addresses:
         #get holder YETI balance
         user_yeti_balance = yeti_balance[address]
-        #check if holder has at least a certain minimum to qualify for reward
-        if user_yeti_balance >= holder_min:
+        #check if holder has a certain minimum to qualify for reward and
+        #if address is not a contract
+        if user_yeti_balance >= holder_min and not address.startswith('con_'):
             #calculate amount to reward
             reward_amount = rewards_token_amount * (user_yeti_balance/100_000_000_000)
             #transfer reward to holder
             reward_token.transfer(amount=reward_amount, to=address)
 
-def assert_owner():
+def assert_operator():
     assert ctx.caller == operator.get(), 'Only operator can call!'
