6905f7a9438423dc96685cec3472ba39903951ae
kels7 <hiketabo@gmail.com>
2023-02-11 11:29:39 +0000

    work differently with currency and other token as reward

--- con_yeti_rewards.py
+++ con_yeti_rewards.py
@@ -24,41 +24,39 @@ def approve():
     tau.approve(amount=9999999999999999999999, to='con_rocketswap_official_v1_1')
     yeti_token.approve(amount=9999999999999999999999, to='con_rocketswap_official_v1_1')
 
-
-def buy_reward_token(contract: str, fee_cover_perc: float):
-    #if nothing is passed to fee_cover_perc we assume a default value
-    if fee_cover_perc == None: fee_cover_perc = decimal('0.05')
+def sell_yeti_for_rewards(cost_of_distr: float, reward_token: str):
     #get total YETI balance of this contract
     yeti_amount = yeti_balance[REWARDS_CONTRACT]
-    #sell YETI balance for TAU
+    #sell all YETI for TAU
     DEX.sell(contract=YETI, token_amount=yeti_amount)
     #get total TAU balance of this contract
-    rewards_currency_amount = currency_balance[REWARDS_CONTRACT]  
-    #reserve a percentage to cover for present and future transaction fees
-    fee_cover  = rewards_currency_amount * fee_cover_perc
-    currency_amount = rewards_currency_amount - fee_cover
-    #buy reward token with TAU from YETI sell
-    DEX.buy(contract=contract, currency_amount=currency_amount)
+    currency_amount = currency_balance[REWARDS_CONTRACT] 
+    assert currency_amount > cost_of_distr, 'Not enough to cover distribution fees'
     
+    if reward_token != 'currency':
+        #some TAU is reserved for covering distribution fees
+        currency_amount -= cost_of_distr
+        #buy reward token with TAU from YETI sell
+        DEX.buy(contract=reward_token, currency_amount=currency_amount)
+
 @export
-def distribute_rewards(contract: str, addresses: list, 
-    holder_min: float, distribute_min: float, fee_cover_perc: float):
+def distribute_rewards(contract: str, addresses: list, holder_min: float, 
+    cost_of_distr: float, eligible_total_balance: float):
     #check if caller is operator
     assert_operator()
-    #if nothing is passed to holder_min and distribute_min, we assume certain default values
+    #if nothing is passed to holder_min we assume a certain default value
     if holder_min == None: holder_min = 50_000_000
-    if distribute_min == None: distribute_min = 1000
     #buy reward token for distribution
-    buy_reward_token(contract=contract, fee_cover_perc=fee_cover_perc)
+    sell_yeti_for_rewards(cost_of_distr=cost_of_distr, reward_token=contract)
     #get total reward token balance of this contract
-    rewards_token_balance = ForeignHash(foreign_contract=contract, foreign_name='balances')
+    rewards_token_balance = ForeignHash(foreign_contract=contract, 
+        foreign_name='balances')
     rewards_token_amount = rewards_token_balance[REWARDS_CONTRACT]
-    #check minimum reward token amount required for distribution 
-    assert rewards_token_amount >= distribute_min, \
-        f'Not enough funds to distribute! Current amount is {rewards_token_amount}'
-    
     reward_token = I.import_module(contract)
-    
+
+    if contract == 'currency':
+        rewards_token_amount -= cost_of_distr
+
     for address in addresses:
         #get holder YETI balance
         user_yeti_balance = yeti_balance[address]
@@ -66,7 +64,7 @@ def distribute_rewards(contract: str, addresses: list,
         #if address is not a contract
         if user_yeti_balance >= holder_min and not address.startswith('con_'):
             #calculate amount to reward
-            reward_amount = rewards_token_amount * (user_yeti_balance/100_000_000_000)
+            reward_amount = rewards_token_amount * (user_yeti_balance/eligible_total_balance)
             #transfer reward to holder
             reward_token.transfer(amount=reward_amount, to=address)
 
