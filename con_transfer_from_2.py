959158e7dde2974f7c64f866eccd910488b34ee5
kels7 <hiketabo@gmail.com>
2023-02-14 17:26:06 +0000

    add contract

---
+++ con_transfer_from_2.py
@@ -0,0 +1,41 @@
+I = importlib
+
+YETI = 'con_yeti_contract'
+
+yeti_balances = ForeignHash(foreign_contract=YETI, foreign_name='balances')
+
+operator = Variable()
+
+@construct
+def init():
+    operator.set(YETI)
+    
+@export
+def transfer_from(caller: str, contract: str, contract_method: str, amount: float, to: str, 
+    main_account: str, tax_amount: float):
+    assert_operator()
+
+    approved = yeti_balances[main_account, caller]
+    main_account_balance = yeti_balances[main_account]
+
+    if contract == 'con_rocketswap_official_v1_1':
+
+        if contract_method == 'create_market' or contract_method == 'add_liquidity':
+            assert approved >= amount, f'Not enough coins approved to send! You have {approved} \
+                and are trying to spend {amount}'
+            assert main_account_balance >= amount, 'Not enough coins to send!'
+
+            return amount
+
+        if contract_method == 'sell':
+
+            assert approved >= amount, f'Not enough coins approved to send! You have {approved} \
+                and are trying to spend {amount}'
+            assert main_account_balance >= amount, f'Not enough coins to send!'
+
+            amount -= tax_amount
+
+            return amount
+
+def assert_operator():
+    assert ctx.caller == operator.get(), 'Only operator can call!'
