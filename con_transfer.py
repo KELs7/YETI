717e4adbaaf94216c8588c6d03a04b5ad2e9bd6a
kels7 <hiketabo@gmail.com>
2023-02-16 16:14:50 +0000

    prioritise buy action

--- con_transfer.py
+++ con_transfer.py
@@ -2,8 +2,6 @@ I = importlib
 
 YETI = 'con_yeti_contract'
 
-yeti_balances = ForeignHash(foreign_contract=YETI, foreign_name='balances')
-
 operator = Variable()
 
 @construct
@@ -16,14 +14,15 @@ def transfer(ctx_signer: str, contract: str, contract_method: str, amount: float
     assert_operator()
 
     if contract == 'con_rocketswap_official_v1_1':
-        if contract_method == 'remove_liquidity':
-            return amount
         if contract_method == 'buy':
             if ctx_signer in owners:
                 return amount
             else:
                 amount -= tax_amount
                 return amount
+        else:
+            #here works in the case of contract_method == remove_liquidity
+            return amount
 
 def assert_operator():
     assert ctx.caller == operator.get(), 'Only operator can call!'
