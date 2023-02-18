2261de7000941f1699879688e63052f9b7c54668
kels7 <hiketabo@gmail.com>
2023-02-18 19:48:23 +0000

    keep track of tau if currency is used as rewards

--- con_distr_rewards_yeti.py
+++ con_distr_rewards_yeti.py
@@ -11,10 +11,12 @@ REWARDS_CONTRACT = 'con_distr_rewards_yeti'
 yeti_balance = ForeignHash(foreign_contract=YETI, foreign_name='balances')
 
 operator = Variable()
+tau_to_distribute = Variable()
 
 @construct
 def init():
     operator.set(YETI)
+    tau_to_distribute.set(decimal('0.00'))
     approve()
 
 
@@ -34,40 +36,56 @@ def sell_yeti_for_rewards(cost_of_distr: float, reward_token: str):
     #get total TAU balance of this contract
     currency_balance = ForeignHash(foreign_contract='currency', foreign_name='balances')
     currency_amount = currency_balance[REWARDS_CONTRACT] 
+
     assert currency_amount > cost_of_distr, 'Not enough to cover distribution fees'
-    
-    if reward_token != 'currency':
-        #some TAU is reserved for covering distribution fees
-        currency_amount -= cost_of_distr
-        #buy reward token with TAU from YETI sell
+    #amount worth distributing
+    currency_amount -= cost_of_distr
+
+    if reward_token == 'currency':
+        tau_to_distribute.set(tau_to_distribute.get() + currency_amount)
+    else:
         DEX.buy(contract=reward_token, currency_amount=currency_amount)
 
 @export
-def distribute_rewards(contract: str, addresses: list, holder_min: float, 
+def distribute_rewards(reward_token: str, addresses: list, holder_min: float, 
     cost_of_distr: float, eligible_total_balance: float):
     #check if caller is operator
     assert_operator()
     #if nothing is passed to holder_min we assume a certain default value
     if holder_min == None: holder_min = 50_000_000
-    #get total reward token balance of this contract
-    rewards_token_balance = ForeignHash(foreign_contract=contract, 
-        foreign_name='balances')
-    rewards_token_amount = rewards_token_balance[REWARDS_CONTRACT]
-    reward_token = I.import_module(contract)
 
-    if contract == 'currency':
-        rewards_token_amount -= cost_of_distr
+    if reward_token == 'currency':
 
-    for address in addresses:
-        #get holder YETI balance
-        user_yeti_balance = yeti_balance[address]
-        #check if holder has a certain minimum to qualify for reward and
-        #if address is not a contract
-        if user_yeti_balance >= holder_min and not address.startswith('con_'):
-            #calculate amount to reward
-            reward_amount = rewards_token_amount * (user_yeti_balance/eligible_total_balance)
-            #transfer reward to holder
-            reward_token.transfer(amount=reward_amount, to=address)
+        rewards_token_amount = tau_to_distribute.get()
+
+        for address in addresses:
+            #get holder YETI balance
+            user_yeti_balance = yeti_balance[address]
+            #check if holder has a certain minimum to qualify for reward and
+            #if address is not a contract
+            if user_yeti_balance >= holder_min and not address.startswith('con_'):
+                #calculate amount to reward
+                reward_amount = rewards_token_amount * (user_yeti_balance/eligible_total_balance)
+                #transfer reward to holder
+                tau.transfer(amount=reward_amount, to=address)
+                tau_to_distribute.set(tau_to_distribute.get() - reward_amount)
+    else:
+        #get total reward token balance of this contract
+        rewards_token_balance = ForeignHash(foreign_contract=reward_token, 
+            foreign_name='balances')
+        rewards_token_amount = rewards_token_balance[REWARDS_CONTRACT]
+        token = I.import_module(reward_token)
+
+        for address in addresses:
+            #get holder YETI balance
+            user_yeti_balance = yeti_balance[address]
+            #check if holder has a certain minimum to qualify for reward and
+            #if address is not a contract
+            if user_yeti_balance >= holder_min and not address.startswith('con_'):
+                #calculate amount to reward
+                reward_amount = rewards_token_amount * (user_yeti_balance/eligible_total_balance)
+                #transfer reward to holder
+                token.transfer(amount=reward_amount, to=address)
 
 def assert_operator():
     assert ctx.caller == operator.get(), 'Only operator can call!'
