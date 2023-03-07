7bd8572333691cef3a0dfadb4813aacba9de603d
kels7 <hiketabo@gmail.com>
2023-03-07 14:24:54 +0000

    reformat

--- con_transfer_from_2.py
+++ con_transfer_from_2.py
@@ -1,41 +1,48 @@
 I = importlib
 
-YETI = 'con_yeti_contract'
+YETI = "con_yeti_contract"
 
 operator = Variable()
+
 
 @construct
 def init():
     operator.set(YETI)
-    
+
+
 @export
-def transfer_from(caller: str, contract: str, contract_method: str, amount: float, to: str, 
-    main_account: str, tax_amount: float):
+def transfer_from(
+    caller: str,
+    contract: str,
+    contract_method: str,
+    amount: float,
+    to: str,
+    main_account: str,
+    tax_amount: float,
+):
     assert_operator()
 
-    yeti_balances = ForeignHash(foreign_contract=YETI, foreign_name='balances')
+    yeti_balances = ForeignHash(foreign_contract=YETI, foreign_name="balances")
 
     approved = yeti_balances[main_account, caller]
     main_account_balance = yeti_balances[main_account]
 
-    if contract == 'con_rocketswap_official_v1_1':
-
-        if contract_method == 'sell':
-
-            assert approved >= amount, f'Not enough coins approved to send! You have {approved} \
-                and are trying to spend {amount}'
-            assert main_account_balance >= amount, f'Not enough coins to send!'
+    if contract == "con_rocketswap_official_v1_1":
+        if contract_method == "sell":
+            assert approved >= amount, f"Not enough coins approved to send! You have {approved} and are trying to spend {amount}"
+            assert main_account_balance >= amount, f"Not enough coins to send!"
 
             amount -= tax_amount
 
             return amount
         else:
-            #here works in the case of contract_method == 'create_market' or contract_method == 'add_liquidity'
-            assert approved >= amount, f'Not enough coins approved to send! You have {approved} \
-                and are trying to spend {amount}'
-            assert main_account_balance >= amount, 'Not enough coins to send!'
+            # here works in the case of contract_method == 'create_market' 
+            # or contract_method == 'add_liquidity'
+            assert approved >= amount, f"Not enough coins approved to send! You have {approved} and are trying to spend {amount}"
+            assert main_account_balance >= amount, "Not enough coins to send!"
 
             return amount
 
+
 def assert_operator():
-    assert ctx.caller == operator.get(), 'Only operator can call!'
+    assert ctx.caller == operator.get(), "Only operator can call!"
